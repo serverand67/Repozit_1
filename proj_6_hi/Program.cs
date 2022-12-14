685b60634113a1414d527f36a57a05a5e0d87bdc
unknown <veronika040182@hotmail.com>
2022-12-14 15:45:28 +0300

    проект 6 привет, есть ошибка- исправить

---
+++ proj_6_hi/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine( "Ввдите ваше имя " );
+string usname = Console.ReadLine();
+if( usname == "маша");
+{
+   Console.WriteLine( "Добро пожаловать Маша" );
+}
+else
+{ 
+   Console.WriteLine( "Привет " );  
+   Console.WriteLine( usname );
+}
+
