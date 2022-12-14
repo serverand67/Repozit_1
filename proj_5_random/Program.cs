8649b8d7464533c773c70345f3ac5178f823d253
unknown <veronika040182@hotmail.com>
2022-12-14 14:15:00 +0300

    проект 5 random

---
+++ proj_5_random/Program.cs
@@ -0,0 +1,6 @@
+﻿double numA = new Random().Next( 1, 5 );
+Console.WriteLine( numA );
+double numB = new Random().Next( 1, 10 );
+Console.WriteLine( numB );
+double result = numA / numB;
+Console.WriteLine( result );
